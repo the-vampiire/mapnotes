4fbd6ffedfa4333f69734bc63dbe38bbb2b9b7f6
the-vampiire <vampiirecodes@gmail.com>
2020-10-15 01:32:50 -0500

    global: prettify!

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import OSMSource from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
 
 const map = new Map({
-    target: "map",
-    view: new View({
-      zoom: 3,
-      center: fromLonLat([-73.60791683207303, -15.595145902766419]),
-    }),
-    layers: [new Tile({ source: new OSMSource() })],
-  });
+  target: "map",
+  view: new View({
+    zoom: 3,
+    center: fromLonLat([-73.60791683207303, -15.595145902766419]),
+  }),
+  layers: [new Tile({ source: new OSMSource() })],
+});
